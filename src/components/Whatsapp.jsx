b248a2de02859970007ae3ad5ab50fcec977590a
jaromtspl <your.email@example.com>
2024-03-04 17:46:08 +0530

    first commit25

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -5,9 +5,9 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 const Whatsapp = () => {
   return (
     <a href="https://wa.me/9894377125?text=Hello!%20I'Am%20Interested%20in%20your%20IT%20services.%20Can%20you%20share%20more%20info,%20please?" target='_blank' rel='noopener noreferrer'>
-      <div className='bg-green-600 rounded-full lg:w-16 lg:h-16 w-16 h-16 flex flex-col items-center justify-center fixed bottom-8 right-6 cursor-pointer z-10'>
+      <div className='bg-green-600 rounded-full lg:w-16 lg:h-16 w-16 h-16 flex flex-col items-center justify-center fixed bottom-8 right-6 cursor-pointer z-10 '>
         <FontAwesomeIcon icon={faWhatsapp} size='2x' className='text-white lg:block hidden'/>
-        <FontAwesomeIcon icon={faWhatsapp} size='2x' className='text-white lg:hidden'/>
+        <FontAwesomeIcon icon={faWhatsapp} size='2x' className='text-white lg:hidden '/>
       </div>
     </a>
   );
